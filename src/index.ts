ba8e14c9ccd2dbe9e7e2f1eb5caf9787311496bd
Ivan Pantic <panta@talentkit.io>
2022-05-29 12:43:26 +0200

    Fix commit types

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
+import { DeepPartial } from 'redux';
+
 import { glrCommit as commit } from './actions';
 import { glrSetErrorHandler as setErrorHandler } from './errors';
 import { glrReducer as reducer } from './reducer';
 import { IActionCommit, IReduxAction } from './types';
-import { GitLikeReduxValue, glrVal as val } from './val';
+import { glrVal as val } from './val';
 
 /**
  * A helper to setup typescript types for all the helpers.
@@ -13,13 +15,13 @@ function glrTyped<TState>(_?: TState): {
   reducer: (state: TState | undefined, action: IReduxAction) => TState;
   commit: (
     message: string,
-    payload: TState,
+    payload: DeepPartial<TState>,
     patch?: object
   ) => IReduxAction & IActionCommit<TState>;
 } {
   return {
     reducer,
-    commit,
+    commit: commit as any,
   };
 }
 
