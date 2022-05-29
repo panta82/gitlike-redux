7fd10329a9003ef7aa02b590cf32295038184586
Ivan Pantic <panta@talentkit.io>
2022-05-29 12:00:47 +0200

    Add NOTE to the commit() command explaining why it works the way it does.

--- src/actions.ts
+++ src/actions.ts
@@ -10,6 +10,24 @@ import { GitLikeReduxValue } from './val';
  *
  * You can also provide "patch", which can be used as an additional, unprocessed part of the payload
  *
+ * NOTE: Instead of sending this object directly to the store, we will convert it into a message object,
+ *       where all the keys are deep paths into the message and values are the wrapped terminators.
+ *       .
+ *       Why? Redux best practice is to only use POJO-s (plain objects) for the action objects and the store itself.
+ *       Various ecosystem middlewares might depend all objects are indeed POJO-s and can be safely
+ *       deserialized / reserialized (although some clearly break this, but then the user is forced
+ *       to carefully position such middlewares - not nice).
+ *       .
+ *       In order to pass unmangled objects, we will have to keep Terminator classes in the payload.
+ *       That might cause unknown conflicts down the line.
+ *       .
+ *       Secondary reason is that, if user fails to wrap the object in val(), we want them to receive
+ *       the error right there, with relevant stack trace, instead of deep down in the store,
+ *       or maybe even in a separate stack frame (in case they are using some kind of a debouncing middleware).
+ *       .
+ *       An advantage of late conversion is probably slightly better performance,
+ *       but IMO it's not worth the risk and ergonomics.
+ *
  * @example
  *     commit("Switch to dark theme", {
  *      user: {
@@ -23,6 +41,8 @@ import { GitLikeReduxValue } from './val';
  *       type: "Switch to dark theme",
  *       "user.profile.theme": "dark"
  *     }
+ *
+ *
  */
 export function glrCommit<TPayload>(
   message: string,
